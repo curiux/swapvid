19b7174c92289ea5838704b16cc3cae7c85fe5b8
curiux <brunocsx32@gmail.com>
2025-05-15 19:16:28 -0300

    fixed uppercase letter on import causing an error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css"
 import Home from "./pages/home.tsx"
-import Register from "./pages/Register.tsx";
+import Register from "./pages/register.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 
 const router = createBrowserRouter([
